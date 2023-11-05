2b1f641f6a2325f9e8ecadeed63fa5a90e687607
ashmit-coder <ashmitjagtap2209@gmail.com>
2023-11-05 12:05:10 +0530

    initial commit

---
+++ database/data.js
@@ -0,0 +1,16 @@
+const pg= require('pg');
+const config = {user:"postgres",password:"ashmit",database:"test",port:5433,host:"localhost"}
+const client = new pg.Client(config);
+client.connect().then(()=>{
+    console.log('Connected');
+});
+
+try {
+    
+    client.query(`SELECT NOW()`).then((res)=>{
+        console.log(res) 
+    });
+    
+ } catch (err) {
+    console.error(err);
+}
